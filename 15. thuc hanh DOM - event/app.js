7980ef9a13fed2e7cf4a5f95a5d21b86e5ec4a3f
Hang Tran <hangtrandhtn@gmail.com>
2023-09-04 23:09:02 +0700

    thuc hanh event

---
+++ 15. thuc hanh DOM - event/app.js
@@ -0,0 +1,41 @@
+// bài 1
+// muốn thực hiện 1 action nào đó thì phải lấy được phần tử đó ra => trường hợp này là Button
+
+
+let liftoffButton = document.querySelector("#liftoffButton")
+liftoffButton.addEventListener("click",()=>{
+const exercise1 = document.querySelector(".exercise-1")
+exercise1.innerText = "Housston, chúng ta đã cất cánh"
+
+});
+
+//bài 2
+let abortMission = document.getElementById("abortMission")
+// console.log("ghjhdf ",abortMission)
+abortMission.addEventListener("mouseover",()=>{
+    abortMission.style.backgroundColor = "red"
+})
+abortMission.addEventListener("mouseout",()=>{
+    abortMission.style.backgroundColor = "rgb(139,220,139)"
+})
+
+
+
+//bài 3 
+let huy = document.getElementById("abortMission1")
+huy.addEventListener("click",()=>{
+let result = confirm("Bạn có muốn hủy chuyến ");
+if(result===true){
+    alert("nhiệm vụ hủy, con tàu thoi đã trở về nhà");
+}
+})
+
+
+// bai 4
+let hienthi = document.getElementById("submit")
+hienthi.addEventListener("click",()=>{
+    let ho = document.getElementById("fristName").value
+    let ten = document.getElementById("lastName").value
+    const text = document.createTextNode(ho + ten)
+   
+})
