2293acc21ebe3481bfd5163db52b2d612ca9ab6b
Hang Tran <hangtrandhtn@gmail.com>
2023-09-05 21:47:38 +0700

    thuc hanh

--- 15. thuc hanh DOM - event/app.js
+++ 15. thuc hanh DOM - event/app.js
@@ -1,41 +1,46 @@
-// bài 1
-// muốn thực hiện 1 action nào đó thì phải lấy được phần tử đó ra => trường hợp này là Button
+// // bài 1
+// // muốn thực hiện 1 action nào đó thì phải lấy được phần tử đó ra => trường hợp này là Button
 
 
-let liftoffButton = document.querySelector("#liftoffButton")
-liftoffButton.addEventListener("click",()=>{
-const exercise1 = document.querySelector(".exercise-1")
-exercise1.innerText = "Housston, chúng ta đã cất cánh"
+// let liftoffButton = document.querySelector("#liftoffButton")
+// liftoffButton.addEventListener("click",()=>{
+// const exercise1 = document.querySelector(".exercise-1")
+// exercise1.innerText = "Housston, chúng ta đã cất cánh"
 
-});
+// });
 
-//bài 2
-let abortMission = document.getElementById("abortMission")
-// console.log("ghjhdf ",abortMission)
-abortMission.addEventListener("mouseover",()=>{
-    abortMission.style.backgroundColor = "red"
-})
-abortMission.addEventListener("mouseout",()=>{
-    abortMission.style.backgroundColor = "rgb(139,220,139)"
-})
-
-
-
-//bài 3 
-let huy = document.getElementById("abortMission1")
-huy.addEventListener("click",()=>{
-let result = confirm("Bạn có muốn hủy chuyến ");
-if(result===true){
-    alert("nhiệm vụ hủy, con tàu thoi đã trở về nhà");
-}
-})
+// //bài 2
+// let abortMission = document.getElementById("abortMission")
+// // console.log("ghjhdf ",abortMission)
+// abortMission.addEventListener("mouseover",()=>{
+//     abortMission.style.backgroundColor = "red"
+// })
+// abortMission.addEventListener("mouseout",()=>{
+//     abortMission.style.backgroundColor = "rgb(139,220,139)"
+// })
 
 
-// bai 4
-let hienthi = document.getElementById("submit")
-hienthi.addEventListener("click",()=>{
-    let ho = document.getElementById("fristName").value
-    let ten = document.getElementById("lastName").value
-    const text = document.createTextNode(ho + ten)
-   
-})
+
+// //bài 3 
+// let huy = document.getElementById("abortMission1")
+// huy.addEventListener("click",()=>{
+// let result = confirm("Bạn có muốn hủy chuyến ");
+// if(result===true){
+//     alert("nhiệm vụ hủy, con tàu thoi đã trở về nhà");
+// }
+// })
+
+
+// // bai 4
+// let hienthi = document.getElementById("submit")
+// hienthi.addEventListener("click",()=>{
+//     const ho = document.getElementById("fristName").value
+//     const ten = document.getElementById("lastName").value
+//     alert(ho + " " + ten);
+// })
+
+// //bai 5
+
+
+
+
